3346351c12939ccc0de6bf4c148e68a2ced6e784
Pradeep Kumar Gupta <120917855+Pradeep-Gupta-96@users.noreply.github.com>
2023-02-02 14:07:28 +0530

    code added

---
+++ i2.js
@@ -0,0 +1,18 @@
+// ==============================reduce the lenght ==============
+// const arr=[1,2,4,5,6,7,4,6,3,2,7,2]
+// arr.length=6
+// console.log(arr)
+
+//===================add all the data==========
+// const arr=[1,2,4,5,6,7,4,6,3,2,7,2,1]
+// console.log(arr.reduce((x,y)=>(x+y)))
+
+//===========remove dublicates================
+// let  arr=[1,2,4,5,6,7,4,6,3,2,7,2,1]
+// let unique=new Set(arr)
+// console.log([...unique])
+
+// ==================comma operator==========
+// let x=3
+// x=(x+=4,x)
+// console.log(x)
